95ef920347170e94da6003364e72ac196522ed4e
Franco Agustín <114515778+FrancooChaparro@users.noreply.github.com>
2023-03-14 16:09:56 -0300

    doneComponents

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -17,8 +17,30 @@ export const Register = () => {
             </div>
             </div>
         </div>
-        <div className={styles.ContainerForm}></div>
-        <div className={styles.ContainerFooter}></div>
+
+        <div className={styles.ContainerForm}>
+              <div className={styles.ContainerFormTop}>
+                  <form action="">
+                      <input type="text" placeholder='Correo Electronico'/>
+                      <input type="text" placeholder='Nombre Completo'/>
+                      <input type="text" placeholder='Nombre de Usuario'/>
+                      <input type="text" placeholder='Contraseña'/>
+                      <div className={styles.ContainerFormMid}><span>Al registrarte, aceptas nuestras Condiciones, nuestra Política de privacidad y nuestra Política de cookies.</span></div>
+                      <div className={styles.ContainerFormBot}>
+                          <button className={styles.btn2}>Registrarse</button>
+                      </div>
+                  </form>
+              </div>
+        </div>
+
+
+        <div className={styles.ContainerFooter}>   
+        <div className={styles.FooterTop}><span>¿Tienes una cuenta? <a href="">Entrar</a></span></div>
+                <div className={styles.FooterBot}>
+                  <div className={styles.ContainerBanner}> <img src="https://es.logodownload.org/wp-content/uploads/2019/06/disponible-en-google-play-badge-1.png" alt="Google" /> </div>
+                  <div className={styles.ContainerBanner}><img src="https://www.rccc.eu/cacheCCC/images/microsoft.png" alt="Microsoft" /> </div>
+                </div>
+        </div>
     </div>
   )
 }
