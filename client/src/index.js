245797ffd9ab42916e407de4bc3fda0e05a7490d
chaparrofranco999 <chaparrofranco999@gmail.com>
2023-03-20 18:51:09 -0300

    Config

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config()
+// import dotenv from "dotenv";
+// dotenv.config()
 
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3002"
